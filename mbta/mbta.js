68450f12fc8621692b1d5d7ea6ee161ff1358767
Billy Witrock <william.witrock@tufts.edu>
2018-10-19 20:12:17 -0400

    getting map to work

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,7 +1,11 @@
 
+var map;
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById("map"), {
                 center: {lat: 42.352271, lng: -71.05524200000001},
                 zoom: 8
         });
+        console.log("in initMap")
+        //var map = document.getElementById("map");
+        //map.innerHTML = "hello";
 }
